77a23a2dbb91df511869f1c6dc09740da920fcbb
KingJango13 <97196637+KingJango13@users.noreply.github.com>
2023-01-10 16:28:50 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 async function getShortToken(authCode){
-    var res = await fetch(`https://api.instagram.com/oauth/access_token?client_id=${localStorage.ig_app_id}&client_secret=83e5d9b15307cb33857609325165c61d&code=${authCode}&grant_type=authorization_code&redirect_uri=https://kingjango13.github.io/JangoMetaTest/auth/`,{
+    var res = await fetch(`https://api.instagram.com/oauth/access_token/`,{
+        body: JSON.stringify({
+            client_id: localStorage.ig_app_id,
+            client_secret: "83e5d9b15307cb33857609325165c61d",
+            code: authCode,
+            grant_type: "authorization_code",
+            redirect_uri: "https://kingjango13.github.io/JangoMetaTest/auth/"
+        });
         method: "POST"
     });
     var data = await res.json();
@@ -8,7 +15,7 @@ async function getShortToken(authCode){
 
 async function getLongToken(shortToken){
     var res = await fetch(`https://graph.instagram.com/access_token?grant_type=ig_exchange_token&client_secret=83e5d9b15307cb33857609325165c61d&access_token=${shortToken}`, {
-        method: "POST"
+        method: "GET"
     });
     var data = await res.json();
     localStorage.ig_token_expiration = Date.now() + (data.expires_in * 1000);
@@ -17,7 +24,7 @@ async function getLongToken(shortToken){
 
 async function refreshToken(){
     var res = await fetch(`https://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token=${localStorage.jango_ig_token}`, {
-        method: "POST"
+        method: "GET"
     });
     var data = await res.json();
     localStorage.ig_token_expiration = Date.now() + (data.expires_in * 1000);
